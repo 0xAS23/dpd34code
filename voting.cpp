c1df1f189488a152e5ad360531b081375d1bb074
aka_askel <70802282+NikosGavriilidis@users.noreply.github.com>
2022-01-27 15:55:50 +0200

    Update source code that i forgot to upload here.
    The code was modified at Mon 10 Jan 2022 07∶45∶33 PM EET and i ended its modification at Sat 01 Jan 2022 07∶46∶11 PM EET.

--- voting.cpp
+++ voting.cpp
@@ -6,45 +6,87 @@ TASK: voting */
 #include <iomanip>
 #include <map>
 #include <iterator>
-
-using namespace std;
-
 int main(){
-    ofstream fout;
-    ifstream fin ;
-    fin.open("voting.in") ;
-    fout.open("voting.out") ;
-  int m ;
-  int n ;
-  int k ;
-
-  fin >> n ;
-  fin >> m ;
-  fin >> k ;
-  map<int, int> list ;
-  for(int i = 0 ; i < m ; i++){
-    list[i] = 0 ;
-
-  }
-  int a ;
-  for(int i = 0 ; i < k ; i++){
-        fin >> a ;
+    freopen ("voting.in","r",stdin);
+    freopen ("voting.out","w",stdout);
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    int m ;
+    int n ;
+    int k ;
+    std::cin >> n ;
+    std::cin >> m ;
+    std::cin >> k ;
+    if( m == 2 ){
+        std::map<int, int> list ;
+    int a ;
+    for(int i = 0 ; i < k ; i++){
+        std::cin >> a ;
         list[a-1]++ ;
-  }
-  signed int max = -1 ;
-  int diff = n - k ;
-  for(int i = 0 ; i < m ; i++){
+    }
+    int max = 0 ;
+    int diff = n - k ;
+    for(int i = 0 ; i < m ; i++){
     if( list[i] > max ){
         max = list[i] ;
+        }
     }
-  }
     int count = 0 ;
     for(int i = 0 ; i < m ; i++){
         if( list[i] + diff > max){
             count++ ;
         }
     }
-    fout << count ;
-    fout.close() ;
-    return 0 ;
+    std::cout << count ;
+    }else if( n == k ){
+        std::map<int,int> list ;
+        int a ;
+    for(int i = 0 ; i < k ; i++){
+        std::cin >> a ;
+        list[a-1]++ ;
+    }
+    int max = 0 ;
+    int pos = 0 ;
+    for(int i = 0 ; i < m ; i++){
+    if( list[i] > max ){
+        max = list[i] ;
+        pos = i ;
+        }
+    }
+    int flag = 1 ;
+    for(int i = 0 ; i < m ; i++){
+        if( i == pos){
+            continue ;
+        }else{
+
+            if( list[i] == max ){
+                flag = 0 ;
+            }
+        }
+    }
+    std::cout << flag ;
+
+    }else{
+        std::map<int, int> list ;
+    int a ;
+    for(int i = 0 ; i < k ; i++){
+        std::cin >> a ;
+        list[a-1]++ ;
+    }
+    int max = 0 ;
+    int diff = n - k ;
+    for(int i = 0 ; i < m ; i++){
+    if( list[i] > max ){
+        max = list[i] ;
+        }
+    }
+    int count = 0 ;
+    for(int i = 0 ; i < m ; i++){
+        if( list[i] + diff > max){
+            count++ ;
+        }
+    }
+    std::cout << count ;
+    }
+
 }
